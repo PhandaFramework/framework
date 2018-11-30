4256f438b2d62e0dcb7df70de791c92f241d82fe
Storm Burpee <storm@phandaframework.com>
2018-12-01 00:09:54 +1030

    starting work on request

--- src/Phanda/Foundation/Http/Request.php
+++ src/Phanda/Foundation/Http/Request.php
@@ -3,9 +3,55 @@
 
 namespace Phanda\Foundation\Http;
 
+use Phanda\Contracts\Support\Arrayable;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
-class Request extends SymfonyRequest
+class Request extends SymfonyRequest implements Arrayable, \ArrayAccess
 {
 
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return [];
+    }
+
+    /**
+     * @param string $offset
+     * @return boolean
+     */
+    public function offsetExists($offset)
+    {
+        return false;
+    }
+
+    /**
+     * @param mixed $offset
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+        return null;
+    }
+
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+
+    }
+
+    /**
+     * @param mixed $offset
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+
+    }
 }
