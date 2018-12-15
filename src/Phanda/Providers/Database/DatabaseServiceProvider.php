a774248d9e4128120e1994f634947c60d322feef
Storm Burpee <storm@phandaframework.com>
2018-12-15 19:18:16 +1030

    Added the mysql database driver to the database repository

--- src/Phanda/Providers/Database/DatabaseServiceProvider.php
+++ src/Phanda/Providers/Database/DatabaseServiceProvider.php
@@ -7,17 +7,25 @@ use Phanda\Contracts\Foundation\Application;
 use Phanda\Database\Connection\Manager as ConnectionManager;
 use Phanda\Contracts\Database\Connection\Manager as ConnectionManagerContract;
 use Phanda\Database\Driver\DriverRegistry;
+use Phanda\Database\Driver\MysqlDriver;
 use Phanda\Providers\AbstractServiceProvider;
 
 class DatabaseServiceProvider extends AbstractServiceProvider
 {
 
+    /**
+     * Registers the classes that handle the Database connections.
+     */
     public function register()
     {
         $this->registerDriverRegistry();
         $this->registerConnectionManager();
     }
 
+    /**
+     * Registers the driver registry which handles the available
+     * database drivers used to connect to the database
+     */
     protected function registerDriverRegistry()
     {
         $this->phanda->singleton('database.driver_registry', function ($phanda) {
@@ -34,6 +42,10 @@ class DatabaseServiceProvider extends AbstractServiceProvider
         $this->phanda->alias('database.driver_registry', DriverRegistry::class);
     }
 
+    /**
+     * Registers the connection manager which handles the currently
+     * active and inactive database connections.
+     */
     protected function registerConnectionManager()
     {
         $this->phanda->singleton('database.connection_manager', function ($phanda) {
@@ -46,14 +58,25 @@ class DatabaseServiceProvider extends AbstractServiceProvider
         $this->phanda->alias('database.connection_manager', ConnectionManager::class);
     }
 
+    /**
+     * Registers all the available database drivers on the
+     * database registry
+     *
+     * @param DriverRegistryContract $driverRegistry
+     */
     protected function registerDrivers(DriverRegistryContract $driverRegistry)
     {
         $this->registerMysqlDriver($driverRegistry);
     }
 
+    /**
+     * Registers the 'mysql' database driver with the registry
+     *
+     * @param DriverRegistryContract $driverRegistry
+     */
     protected function registerMysqlDriver(DriverRegistryContract $driverRegistry)
     {
-
+        $driverRegistry->registerDriver('mysql', MysqlDriver::class);
     }
 
 }
