9cbd2ad2f38f279efc22fe828b0767ba3aa6cd52
Storm Burpee <storm@phandaframework.com>
2018-12-16 20:40:50 +1030

    Added the TableRepository registration to the BearServiceProvider

--- src/Phanda/Providers/Bear/BearServiceProvider.php
+++ src/Phanda/Providers/Bear/BearServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Phanda\Providers\Bear;
 
+use Phanda\Bear\Table\Table;
+use Phanda\Contracts\Bear\Table\TableRepository;
 use Phanda\Providers\AbstractServiceProvider;
 
 use Phanda\Contracts\Bear\Query\Builder as QueryBuilderContact;
@@ -10,17 +12,33 @@ use Phanda\Bear\Query\Builder as QueryBuilder;
 class BearServiceProvider extends AbstractServiceProvider
 {
 
-    /**
-     * Registers the BearORM and it's related classes
-     */
-    public function register()
-    {
-        $this->registerBearQueryBuilder();
-    }
+	/**
+	 * Registers the BearORM and it's related classes
+	 */
+	public function register()
+	{
+		$this->registerBearQueryBuilder();
+		$this->registerBearTable();
+	}
 
-    public function registerBearQueryBuilder()
-    {
-        $this->phanda->attach(QueryBuilderContact::class, QueryBuilder::class);
-    }
+	/**
+	 * Registers the bear QueryBuilder, so that it can resolve whenever it is type hinted.
+	 */
+	public function registerBearQueryBuilder()
+	{
+		$this->phanda->attach(QueryBuilderContact::class, QueryBuilder::class);
+	}
+
+	/**
+	 * Registers the Bear Table, so that it resolves whenever it is type hinted.
+	 */
+	public function registerBearTable()
+	{
+		$this->phanda->attach(TableRepository::class, function () {
+			return new Table();
+		});
+
+		$this->phanda->alias(TableRepository::class, Table::class);
+	}
 
 }
